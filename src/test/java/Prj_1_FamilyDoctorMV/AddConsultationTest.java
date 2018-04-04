700d8102afd31a11c7da94250965607e43962bee
mirko <mirceadino97@gmail.com>
2018-04-04 10:30:43 +0300

    Tests for consultation

--- src/test/java/Prj_1_FamilyDoctorMV/AddConsultationTest.java
+++ src/test/java/Prj_1_FamilyDoctorMV/AddConsultationTest.java
@@ -1,24 +1,26 @@
 package Prj_1_FamilyDoctorMV;
 
 import Prj_1_FamilyDoctorMV.controller.DoctorController;
-import Prj_1_FamilyDoctorMV.exceptions.PatientException;
-import Prj_1_FamilyDoctorMV.model.Patient;
+import Prj_1_FamilyDoctorMV.exceptions.ConsultationException;
 import Prj_1_FamilyDoctorMV.repository.Repository;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
-public class AddPatientTest
+public class AddConsultationTest
         extends TestCase {
   /**
    * Create the test case
    *
    * @param testName name of the test case
    */
-  public AddPatientTest(String testName) {
+  public AddConsultationTest(String testName) {
     super(testName);
   }
 
@@ -26,35 +28,36 @@ public class AddPatientTest
    * @return the suite of tests being tested
    */
   public static Test suite() {
-    return new TestSuite(AddPatientTest.class);
+    return new TestSuite(AddConsultationTest.class);
   }
 
   /**
    * Rigourous Test :-)
    */
-  public void testAddValidPatient() {
+  public void testAddNullMedsConsultation() {
     Repository repo = new Repository("mock-FilePatients.txt", "mock-FileConsultations.txt");
     DoctorController ctrl = new DoctorController(repo);
     repo.cleanFiles();
 
     try {
-      ctrl.addPatient(new Patient("George Hora", "1900505050005", "9 George Hora"));
-      assertEquals(ctrl.getPatientList().size(), 1);
-    } catch (PatientException err) {
+      ctrl.addConsultation("", "", "", null, "");
       assertTrue(false);
+    } catch (ConsultationException err) {
+      assertEquals(0, ctrl.getConsultationList().size());
     }
   }
 
-  public void testAddInvalidSsnPatient() {
+  public void testAddInvalidArgumentsConsultation() {
     Repository repo = new Repository("mock-FilePatients.txt", "mock-FileConsultations.txt");
     DoctorController ctrl = new DoctorController(repo);
     repo.cleanFiles();
 
     try {
-      ctrl.addPatient(new Patient("Mihai Radu", "invalid", "9 Mihai Radu"));
+      List<String> meds = new ArrayList<String>();
+      ctrl.addConsultation("11", "11", "11", meds, "11.01.2011");
       assertTrue(false);
-    } catch (PatientException err) {
-      assertEquals(ctrl.getPatientList().size(), 1);
+    } catch (ConsultationException err) {
+      assertEquals(0, ctrl.getConsultationList().size());
     }
   }
 }
